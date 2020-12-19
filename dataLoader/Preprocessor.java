1c026c9801ff2565bc01f9c6031472020a8e31cd
Shravan Ravi <36645942+ShravanRavi2002@users.noreply.github.com>
2020-12-19 09:47:06 -0600

    Finalized Java Backend processes to create FIrebase collection

--- dataLoader/Preprocessor.java
+++ dataLoader/Preprocessor.java
@@ -1,11 +1,12 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
-
-import org.json.simple.*;
-import org.json.simple.parser.*;
 
 
 public class Preprocessor {
@@ -13,15 +14,16 @@ public class Preprocessor {
     public static void main(String[] args) {
         getAirportLocation();
     }
-
-    public static TreeMap<String, TreeSet<String>> preprocess () {
+    
+    public static TreeMap<String, TreeSet<String>> preprocess() {
         TreeMap<String, TreeSet<String>> routes = new TreeMap<>();
         parseData(routes);
         getAirportLocation();
         return routes;
     }
 
-    private static void parseData (TreeMap<String, TreeSet<String>> routes) {
+    // Read the routes data and creates a TreeMap containing all the connections for a given airport
+    private static void parseData(TreeMap<String, TreeSet<String>> routes) {
         File file = new File("dataLoader/routes.txt");
         try (Scanner sc = new Scanner(file, StandardCharsets.UTF_8.name())) {
             while (sc.hasNextLine()) {
@@ -39,6 +41,8 @@ public class Preprocessor {
         }
     }
 
+    // Reads through the state_abbreviations.json to translate state names to
+    // their respective abbreviation in a HashMap
     private static HashMap<String, String> getAbbreviations() {
         File file = new File("dataLoader/state_abbreviations.json");
         HashMap<String, String> abbreviations = new HashMap<>();
@@ -46,7 +50,7 @@ public class Preprocessor {
             while (sc.hasNextLine()) {
                 String next = sc.nextLine();
                 String[] abbrevs = next.split(": ");
-                abbreviations.put(abbrevs[0], abbrevs[1].substring(1,3));
+                abbreviations.put(abbrevs[0], abbrevs[1].substring(1, 3));
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -54,27 +58,26 @@ public class Preprocessor {
         return abbreviations;
     }
 
+    // Reads through the airports text file to create a mapping from airport code to state name
     public static HashMap<String, String> getAirportLocation() {
-
         HashMap<String, String> airportLocations = new HashMap<>();
         HashMap<String, String> abbrevs = getAbbreviations();
         JSONParser parser = new JSONParser();
         try {
             Object obj = parser.parse(new FileReader("dataLoader/airports.json"));
-            JSONArray airports = (JSONArray)obj;
+            JSONArray airports = (JSONArray) obj;
             Iterator iterator = airports.iterator();
-            while(iterator.hasNext()) {
-                JSONObject airport = (JSONObject)iterator.next();
-                String code = (String)airport.get("code");
-                String state = (String)airport.get("state");
-                if(abbrevs.containsKey(state)) {
+            while (iterator.hasNext()) {
+                JSONObject airport = (JSONObject) iterator.next();
+                String code = (String) airport.get("code");
+                String state = (String) airport.get("state");
+                if (abbrevs.containsKey(state)) {
                     airportLocations.put(code, abbrevs.get(state));
-                } else {
+                } else if (state != null) {
                     airportLocations.put(code, "INTERNATIONAL");
                 }
             }
-
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return airportLocations;
--- dataLoader/Uploader.java
+++ dataLoader/Uploader.java
@@ -17,16 +17,17 @@ import java.util.TreeSet;
 
 public class Uploader {
 
+    private static final boolean DEBUG = false;
     private static TreeMap<String, TreeSet<String>> routes;
     private static HashMap<String, String> airportLocations;
     private static Firestore db;
-    private static final boolean DEBUG = false;
 
-    public static void main (String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         upload();
     }
 
-    public static void upload () throws IOException {
+    // loads airport data into Firebase
+    public static void upload() throws IOException {
         getRoutes();
         getLocations();
         initDB();
@@ -34,49 +35,50 @@ public class Uploader {
 
         debug("Adding all routes...");
 
-        CollectionReference colRef = db.collection("connections");
+        CollectionReference colRef = db.collection("airports");
         for (String route : routes.keySet()) {
             HashMap<String, Object> fields = new HashMap<>();
-            fields.put("flights", new ArrayList<>(routes.get(route)));
-            fields.put("state", airportLocations.get(route));
-            ApiFuture<WriteResult> result = colRef.document(route).set(fields);
-            while (!result.isDone()) {
-                // hi! if you're reading this, we apologize
-                // this ensures we wait until the request is fulfilled.
-                continue;
+            if (airportLocations.containsKey(route)) {
+                fields.put("connections", new ArrayList<>(routes.get(route)));
+                fields.put("state", airportLocations.get(route));
+                ApiFuture<WriteResult> result = colRef.document(route).set(fields);
+                while (!result.isDone()) {
+                    // hi! if you're reading this, we apologize
+                    // this ensures we wait until the request is fulfilled.
+                    continue;
+                }
+                debug("\tAdded " + route);
             }
-            debug("\tAdded " + route);
         }
 
         debug("Added all valid routes.");
     }
 
-    private static void initDB () throws IOException {
+    // Authorities and initializes our Firebase Database
+    private static void initDB() throws IOException {
         InputStream serviceAccount = new FileInputStream("dataLoader/service_account_credentials.json");
         GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(credentials)
                 .build();
         FirebaseApp.initializeApp(options);
-
         db = FirestoreClient.getFirestore();
     }
 
-    private static void getRoutes () {
+    private static void getRoutes() {
         debug("Preprocessing data...");
         routes = Preprocessor.preprocess();
         debug("Preprocessed data.");
     }
 
-    private static void getLocations () {
+    private static void getLocations() {
         debug("Getting abbreviations...");
         airportLocations = Preprocessor.getAirportLocation();
         debug("Stored abbreviations.");
     }
 
-    private static void debug (String text) {
+    private static void debug(String text) {
         if (DEBUG)
             System.out.println(text);
     }
-
 }
